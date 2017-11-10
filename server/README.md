f462bafb16ac38dda2f03ec1fa68509448d837d5
colton <Colton Morris>
2017-11-10 09:42:15 -0700

    chatbot set up with local configs

--- server/README.md
+++ server/README.md
@@ -1,3 +1,12 @@
-# TODO
+# Server
+This portion of our code is a node.js webserver that is connected to twitch's IRC. It updates a firebase db with new commands said in the chat.
 
-* Make the server :)
+### To Run
+``` bash
+npm run start
+```
+
+### TODO
+* have a handler file with all available commands
+* help command
+* connect to firebase
---
+++ server/app.js
@@ -0,0 +1,49 @@
+let config = require('config')
+let tmi = require('tmi.js')
+
+//defualt options for tmi.js
+let options = {
+	options: {
+		debug: true
+	},
+	connections: {
+		cluster: "aws",
+		reconnect: true
+	},
+	identity: {
+		username: config.get('botName'),
+		password: config.get('password')
+	},
+	channels: [config.get('initialChannel')]
+}
+
+//connect to irc
+let client = new tmi.client(options)
+client.connect()
+
+let initialChannel = config.get('initialChannel')
+
+//change the color of username, doesnt work??
+client.color("HotPink")
+
+client.on('chat',function(channel,user,message,self) {
+	if (message === "!joke") {
+		//after !joke is said, switches channel bot is talking in to kingrazy
+		client.action(initialChannel, "You are the joke.")
+		// client.part(initialChannel)
+		// initialChannel = "kingrazy"
+		// client.join("kingrazy")
+		// client.action(initialChannel, user['display-name'] + " hi there whats up dawgg.")
+	}
+	else{
+		client.action(initialChannel, user['display-name'] + " hi there whats up dawgg.")
+	}
+})
+
+client.on('connected',function(address, port) {
+	client.action(initialChannel, "hello im a cool guy")
+})
+
+
+
+
