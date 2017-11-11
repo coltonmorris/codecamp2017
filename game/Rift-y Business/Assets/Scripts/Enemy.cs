e48e0cb3c3f849812977d3480116c93e461bd9df
Aaron Schafer <hashtaggigs@users.noreply.github.com>
2017-11-10 17:00:39 -0700

    Added scripts, prefabs, material

---
+++ game/Rift-y Business/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+
+	void Start () {
+		target = EnemyTarget.enemyTarget[0];
+	}
+
+	void Update () {
+		Vector3 dir = target.position - transform.position;
+		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
+
+		// if enemy gets to player
+		if (Vector3.Distance (transform.position, target.position) <= 0.4f) {
+			// die!
+			Destroy(gameObject);
+			return;
+		}
+	}
+}
