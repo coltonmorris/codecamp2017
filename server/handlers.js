0bdfefcdfed087ddc5e450ccddd7f9e0837cbc01
colton <Colton Morris>
2017-11-11 05:48:21 -0700

    made sure bad values arent added

--- server/handlers.js
+++ server/handlers.js
@@ -32,20 +32,29 @@ commands['!inc'] = {
 }
 
 commands['!object'] = {
-  'info': '!object: This creates a object with an x and z coordinate. Must be between 0-50. Example usage: "!object 20 30".',
+  'info': '!object: This creates a object with an x and z coordinate. Must be between -50 to 50 (can\'t be too close though). Example usage: "!object 20 30".',
   'run': (user, message) => {
     let max = 50
-    let min = 0
+    let min = -50
 
     if (message.length != 3) {
       return ['POOR USAGE. It should look like this: "!object <num> <num>"']
     }
 
-    let x = parseInt(message[1]) || -1
-    let z = parseInt(message[2]) || -1
+    let x = parseInt(message[1]) || min -1
+    let z = parseInt(message[2]) || min -1
+    // parseInt doesn't work with 0... haha KMS
+    if (message[1] == '0') x = 0
+    if (message[2] == '0') z = 0
 
     // check for NaN and for too large
-    if (x < min || x > max || z < min || z > max) return ['POOR USAGE. It should be between 0-50. Example: "!object 10 20"']
+    if (x < min || x > max || z < min || z > max) return ['POOR USAGE. It should be between -50 to 50. Example: "!object -10 20"']
+
+    // Make sure we do not spawn an object right on the player...
+    if (x < 20 && x > 0) x = 20
+    if (x > -20 && x < 0) x = -20
+    if (z < 20 && z > 0) z = 20
+    if (z > -20 && z < 0) z = -20
 
     firebase.app().database().ref('badguys/').push({
       x: x,
